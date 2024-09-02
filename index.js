a970e581572a1b5d3104d208533d35b3dc361261
Pham Minh Thong (Juu) <103202716+Juu-dev@users.noreply.github.com>
2024-09-02 13:15:28 +0700

    Check vercel auto deploy

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/test-production', (req, res) => {
+  res.send('Production success!');
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
